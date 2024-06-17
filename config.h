3537e2c3b9345d61fa44093c6df176ac48942c2c
André Nascimento <andre.neospace@gmail.com>
2024-06-16 22:29:12 -0300

    defines

---
+++ config.h
@@ -0,0 +1,14 @@
+#pragma once
+
+/*
+* definitions about the program
+*/
+
+// max of 10,000 characters per line
+#define MAX_LINE_SIZE 10000
+
+// max of 5,000 lines
+#define MAX_LINES 5000
+
+// return CTRL + X
+#define  CTRL_KEY(k) ((k) & (0x1f))
