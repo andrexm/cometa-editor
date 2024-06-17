1c146dfd393165d9d025079afa6d7963b5882af5
André Nascimento <andre.neospace@gmail.com>
2024-06-16 23:59:22 -0300

    basic skeleton

--- main.c
+++ main.c
@@ -1,6 +1,64 @@
 #include <ncurses.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+#include "config.h"
+#include "files.h"
+
+
+// exit the program when an error is found
+void die(char *msg) {
+  perror(msg);
+  exit(1);
+}
+
+
+void colorGreen() {
+  start_color();
+  init_pair(1, COLOR_GREEN, COLOR_BLACK);
+  attron(COLOR_PAIR(1));
+}
+
+void stopColorGreen() {
+  attroff(COLOR_PAIR(1));
+}
+
+void initEditor() {
+  editor_info.opened_files_amount = 0;
+  colorGreen();
+}
 
 int main() {
-  
+  char c;
+
+  initscr();
+  noecho();
+  raw();
+  keypad(stdscr, true);
+  refresh();
+
+  initEditor();
+  clear();
+
+  if (!openFile("t.txt")) {
+    printw("error while reading file!");
+    getch();
+    return 1;
+  }
+
+  while (1) {
+    c = getch();
+    if (c == CTRL_KEY('q')) {
+      printw("closing?\r\n");
+      refresh();
+      closeFiles();
+      break;
+    }
+
+    if (c == 'c') {
+      clearLine(0, &files[0]);
+    }
+  }
+
+  endwin();
 }
